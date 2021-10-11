433d5a3be1e288367c40f792fbec7eec08090bf2
Taissiya Ugay <taissiya.ugay@gmail.com>
2021-10-10 22:01:03 -0500

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const studs = ['1', '2', '3', '4', '5', '6']
 //change the name of checkpoint you're going to check
 const checkpoint = 'Checkpoint-React-v2'
-const { mkdir } = require('fs');
 //dont forget to npm i shelljs
 const shell = require('shelljs')
 
