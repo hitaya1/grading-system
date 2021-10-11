709c56631373426690545167ae83d21db9976376
Taissiya Ugay <taissiya.ugay@gmail.com>
2021-10-10 22:00:17 -0500

    small fixes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//add your students git handles
+//add your students github handles
 const studs = ['1', '2', '3', '4', '5', '6']
 //change the name of checkpoint you're going to check
 const checkpoint = 'Checkpoint-React-v2'
@@ -6,7 +6,7 @@ const { mkdir } = require('fs');
 //dont forget to npm i shelljs
 const shell = require('shelljs')
 
-//
+
 for (let stud of studs) {
   shell.exec(`git clone git@github.com:${stud}/${checkpoint}.git ${stud}`)
 }
