60cee773994992b88c9458db813f80895af97beb
Taissiya Ugay <taissiya.ugay@gmail.com>
2021-10-13 13:40:29 -0500

    updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //add your students github handles
-const studs = ['1', '2', '3', '4', '5', '6']
+const studs = ['LABerk', 'paigekelsey', 'Kongyuechen', 'JeffreyLWood', 'davdli', 'gregbabbert']
 //change the name of checkpoint you're going to check
 const checkpoint = 'Checkpoint-React-v2'
 //dont forget to npm i shelljs
